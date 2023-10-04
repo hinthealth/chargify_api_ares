897caebfa88b88df8ea8c3294ccb5eecdfefb501
Matias Nieves <matinieves@gmail.com>
2023-10-04 12:53:22 -0300

    Address comments.

--- lib/chargify_api_ares/resources/component_price_point.rb
+++ lib/chargify_api_ares/resources/component_price_point.rb
@@ -11,13 +11,13 @@ module Chargify
 
     def self.find_subscription_price_point(component_id, subscription_id)
       price_points = all(component_id)
-      subscription_price_point = find_by_attribute(price_points, :subscription_id, subscription_id)
+      subscription_price_point = find_by_subscription_id(price_points, subscription_id)
 
       return subscription_price_point if subscription_price_point
 
       component = Chargify::Subscription.new({ id: subscription_id }).component(component_id)
 
-      find_by_attribute(price_points, :id, component.price_point_id)
+      find_by_id(price_points, component.price_point_id)
     end
 
     def self.create(component_id, price_point_attrs)
@@ -28,10 +28,16 @@ module Chargify
       put :default
     end
 
-    def self.find_by_attribute(price_points, attr_name, attr_value)
-      price_points.find { |price_point| price_point.try(attr_name) == attr_value }
+    def self.find_by_subscription_id(price_points, subscription_id)
+      price_points.filter { |price_point| price_point.try(:subscription_id) == subscription_id }.sort_by(&:id).last
     end
 
-    private_class_method :find_by_attribute
+    private_class_method :find_by_subscription_id
+
+    def self.find_by_id(price_points, id)
+      price_points.find { |price_point| price_point.id == id }
+    end
+
+    private_class_method :find_by_id
   end
 end
